fe06ac136b9f0f37b634b68ece73dcd838814e35
Khabib Gadzhimagomedov <khabib.x.gadzhimagomedov@gmail.com>
2026-08-02 02:20:38 +0300

    feat(auth): add auth slice with user state and loading status

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,9 +1,18 @@
+import { authReducer, setUser } from '@/features/auth/model/authSlice';
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: {
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [setUser.type],
+        ignoredPaths: ['auth.user'],
+      },
+    }),
 });
 
 setupListeners(store.dispatch);
